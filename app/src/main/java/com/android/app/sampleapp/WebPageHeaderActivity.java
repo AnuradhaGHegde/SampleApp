aea8ed4bd7f9b0c2c45750c3afacb3e57a6a1ce9
abhay.student <abhay.27@gmail.com>
2018-08-03 19:30:13 -0700

    add scrollbar functionality

--- app/src/main/java/com/android/app/sampleapp/WebPageHeaderActivity.java
+++ app/src/main/java/com/android/app/sampleapp/WebPageHeaderActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.TextView;
@@ -42,6 +43,7 @@ public class WebPageHeaderActivity extends AppCompatActivity {
 
 
         TextView webheaderTxtVw = (TextView) findViewById(R.id.webHeaderTxtVw);
+        webheaderTxtVw.setMovementMethod(new ScrollingMovementMethod());
         webheaderTxtVw.setText(headerData);
 
     }
